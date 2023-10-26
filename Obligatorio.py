0ba927731af9edfd86f300182f8011a55187341b
Nico Cygan <nicocygan18@gmail.com>
2023-10-26 16:30:19 -0300

    Inicio

---
+++ Obligatorio.py
@@ -0,0 +1,32 @@
+class Empleado:
+      def __init__(self, id, nombre, edad, nacionalidad, fecha_nacimiento, salario) -> None:
+            self.id = id 
+            self.nombre = nombre 
+            self.edad = edad 
+            self.nacionalidad = nacionalidad 
+            self.fecha_nacimiento = fecha_nacimiento
+            self.salario = salario 
+# #MAIN
+# while True:
+#     print("\nIngrese el número de la operación que desea ejecutar:")
+#     print("1 - Alta de empleado")
+#     print("2 - Alta de auto")
+#     print("3 - Alta de equipo")
+#     print("4 - Simular carrera")
+#     print("5 - Realizar consultas")
+#     print("6 - Finalizar programa")
+#     opcion = input(">>>")
+#     if opcion == "1":
+
+#     elif opcion == "2":
+
+#     elif opcion == "3":
+
+#     elif opcion == "4":
+
+#     elif opcion == "5":
+
+#     elif opcion == "6":
+#         break
+#     else: 
+#          print ("Opción inválida")
