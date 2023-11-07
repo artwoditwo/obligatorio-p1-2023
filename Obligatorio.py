f5be693e274ed46d450110e0d056b124abbd9add
secrom36 <smartvisioniep@gmail.com>
2023-11-07 20:10:41 -0300

    a

--- Obligatorio.py
+++ Obligatorio.py
@@ -8,9 +8,9 @@ from entities.equipo import Equipo
 # #MAIN
 # while True:
 #     print("\nIngrese el número de la operación que desea ejecutar:")
-#     print("1 - Alta de empleado")
-#     print("2 - Alta de auto")
-#     print("3 - Alta de equipo")
+#     print("1 - Agregar de empleado")
+#     print("2 - Agregar de auto")
+#     print("3 - Agregar de equipo")
 #     print("4 - Simular carrera")
 #     print("5 - Realizar consultas")
 #     print("6 - Finalizar programa")
