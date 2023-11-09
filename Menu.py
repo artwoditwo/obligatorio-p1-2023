e68805c1299fffcd117e15128bfb775140c7563d
secrom36 <smartvisioniep@gmail.com>
2023-11-09 18:37:44 -0300

    cambio de nombre Obligatorio-Menu creacion de la clase controladora menu

---
+++ Menu.py
@@ -0,0 +1,14 @@
+from entities.empleado import Empleado
+from entities.piloto import Piloto
+from entities.mecanico import Mecanico
+from entities.director_equipo import Director_equipo
+from entities.auto import Auto
+from entities.equipo import Equipo
+
+class Menu():
+    def _init__(self):
+        self._lista_empleados=[]
+        self._lista_auto=[]
+        self._lista_empleados=[]
+        
+        
