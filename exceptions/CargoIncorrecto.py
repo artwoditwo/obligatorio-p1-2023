f32ce598fe86dc7a6c63b5284042e60f5cccb1f2
secrom36 <smartvisioniep@gmail.com>
2023-11-10 22:23:52 -0300

    asd

--- exceptions/CargoIncorrecto.py
+++ exceptions/CargoIncorrecto.py
@@ -1,2 +1,9 @@
 class CargoIncorrecto(Exception):
-        pass
+        def __init__(self, codigo, descripcion):
+                self.codigo = codigo
+                self.descripcion = descripcion
+
+        #luego cuando usamos el raise, pones raise CargoIncorrect(404,"el cargo del empleado es incorrecto")
+        #lo otro seria cambiar todos los try y exception en uno solo
+        #try:
+        #exception CargoIncorrecto or CInoExiste or...... as e:
